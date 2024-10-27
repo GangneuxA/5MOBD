091ffed61e1926e819340ad5ad51756c0e0d1887
GangneuxA <gangneux.alex@gmail.com>
2024-10-27 15:33:46 +0100

    update

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -4,12 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC2MtR7Z6ybuua7ewi974OI2pNd314tfPI",
-    authDomain: "mesbonnesadresses-f0067.firebaseapp.com",
-    projectId: "mesbonnesadresses-f0067",
-    storageBucket: "mesbonnesadresses-f0067.appspot.com",
-    messagingSenderId: "998670254067",
-    appId: "1:998670254067:web:2f280d30368235c5991c86"
+    
 };
   
 const app = initializeApp(firebaseConfig);
